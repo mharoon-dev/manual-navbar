323588a94045d946f8d85e59a0064849706d4370
mharoon-dev <haroon.aghani@gmail.com>
2023-10-21 01:03:55 +0500

    styling

---
+++ app.js
@@ -0,0 +1,20 @@
+// let webPages = document.querySelector(".webPages")
+// let leftSide = document.querySelector(".leftSide")
+// let togglerBtn = document.querySelector(".togglerBtn")
+
+
+// function navbar() {
+//     const windowWidth = window.innerWidth;
+//     if (windowWidth <= 992 ) {
+//         webPages.style.display = "none";
+//         leftSide.style.display = "none";
+//         togglerBtn.style.display = "block";
+//     } else {
+//         webPages.style.display = "block";
+//         leftSide.style.display = "flex";
+//         togglerBtn.style.display = "none";
+//     }
+//   }
+// navbar();
+
+// window.addEventListener('resize', navbar);
